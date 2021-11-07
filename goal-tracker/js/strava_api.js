e2205ca40206cd6c376137da3a507ca67a5304ba
ogladfelter <ogladfelter@gmail.com>
2021-11-07 15:57:09 -0600

    use same API strucutre as the heatmap

--- goal-tracker/js/strava_api.js
+++ goal-tracker/js/strava_api.js
@@ -1,3 +1,50 @@
+strava_data = [];
+
+///////////////////////////////////// API token setting up ///////////////////////////////////////////////////
+// check for URL params
+var queryString = window.location.search;
+
+// search local storage for a possible access token
+try{
+    var token_exists = JSON.parse(localStorage.getItem("strava_data")).access_token != null;
+    var expires_at = JSON.parse(localStorage.getItem("strava_data")).expires_at;
+}
+catch{
+    var token_exists = false;
+    var expires_at = 0;
+}
+
+// 4 possible scenarios: token in local storage, expired token in local storage, no token and no oauth code, no token but oauth code
+if (token_exists & (new Date().getTime() / 1000) < expires_at) { // their access token is saved in local storage and hasn't expired yet
+    console.log("Token exists");
+
+    updateAthleteInfo(JSON.parse(localStorage.getItem("strava_data")).athlete);
+
+    token = JSON.parse(window.localStorage.getItem("strava_data")).access_token;
+    getActivities(1);
+}
+else if (token_exists & (new Date().getTime() / 1000) >= expires_at) { // their access token is saved in local storage but expired
+    console.log("Previous authorization detected; access token is expired");
+
+    updateAthleteInfo(JSON.parse(localStorage.getItem("strava_data")).athlete);
+    
+    var refreshToken = JSON.parse(localStorage.getItem("strava_data")).refresh_token;
+    reAuthorize(refreshToken);
+}
+else if (queryString == "" || queryString == "?state=&error=access_denied"){ // we don't have a code. They still need to log in and authorize
+    // encourage them to log in and authorize
+    console.log("No token in local storage, no authorization code");
+    //document.getElementById("logInModal").style.display = "block";
+}
+else{ // we have a code because they logged in and authorized. the code can be found in the URL params
+    console.log("Authorization code retrieved");
+
+    var urlParams = new URLSearchParams(queryString);
+    var code = urlParams.get('code');
+    get_token();
+}
+
+////////////////////////////////// the functions /////////////////////////////////////////////////////////////
 function get_token(){
 
     // use code from authorization to get user token
@@ -21,17 +68,13 @@ function get_token(){
             // save token
             token = response.access_token;
 
-            // update front end stuff with their profile info
-            updateAthleteInfo(response.athlete);
-
             // get 30 activities
-            getActivities();   
+            getActivities(1);   
         }
     });
 
 }
 
-//  THIS NEEDS TO BE IN PHP TO HIDE MY CLIENT SECRET TOO
 function reAuthorize(refreshToken){
 
     // use code from authorization to get user token
@@ -50,35 +93,44 @@ function reAuthorize(refreshToken){
             token = response.access_token;
 
             // get 30 activities
-            getActivities();
-            
+            getActivities(1);
         }
     });  
+}
+
+function getActivities(pageNum){
+    
+    console.log(pageNum);
+
+    const activities_link = `https://www.strava.com/api/v3/activities?per_page=200&access_token=` + token + "&page=" + pageNum;
+    fetch(activities_link)
+    .then(response => response.json())
+    .then((json) => {
+
+        // if this page had 200 activities, then there's likely another page
+        if (json.length == 200){
+            strava_data = strava_data.concat(json); // save contents from this page, run another call to the next page
+            getActivities(pageNum + 1); // recursion!!
+        }
+        else if (json.length < 200){ // we've reached the final page
+            strava_data = strava_data.concat(json); // save final page's activities to main data array and move on
+
+            // some data prep
+            strava_data.forEach(function(d){ 
+                d.year = d.start_date.slice(0,4);
+                d.miles = +d.distance / 1609.34; 
+            })
+
+            console.log(strava_data);
+
+            renderDashboard(strava_data);
+        }
+    })
 }
 
 function updateAthleteInfo(data){
     document.getElementById("name").innerHTML = data['firstname'] + "'s 2021 Mileage";
     document.getElementById("profile_picture").src = data['profile_medium'];
-}
-
-function getActivities() {
-
-    document.getElementById("loader").style.display = 'block';
-    
-    const activities_link = `https://www.strava.com/api/v3/activities?per_page=200&access_token=` + token;
-    fetch(activities_link)
-    .then(response => response.json())
-    .then((json) => {
-        activityData = json;
-        
-        // some data prep
-        activityData.forEach(function(d){ 
-            d.year = d.start_date.slice(0,4);
-            d.miles = +d.distance / 1609.34; 
-        })
-
-        renderDashboard(activityData);
-    })
 }
 
 function renderDashboard(activityData) {
@@ -92,45 +144,3 @@ function renderDashboard(activityData) {
 d3.json("data.json", function(error, data) {
     renderDashboard(data);
 });
-
-///////////////////////////////////// API token setting up ///////////////////////////////////////////////////
-// check for URL params
-var queryString = window.location.search;
-
-// search local storage for a possible access token
-try {
-    var token_exists = JSON.parse(localStorage.getItem("strava_data")).access_token != null;
-    var expires_at = JSON.parse(localStorage.getItem("strava_data")).expires_at;
-}
-catch {
-    var token_exists = false;
-    var expires_at = 0;
-}
-
-// 4 possible scenarios: token in local storage, expired token in local storage, no token and no oauth code, no token but oauth code
-if (token_exists & (new Date().getTime() / 1000) < expires_at) { // their access token is saved in local storage and hasn't expired yet
-    console.log("Token exists");
-    token = JSON.parse(window.localStorage.getItem("strava_data")).access_token;
-    
-    updateAthleteInfo(JSON.parse(localStorage.getItem("strava_data")).athlete);
-
-    getActivities();
-}
-else if (token_exists & (new Date().getTime() / 1000) >= expires_at) { // their access token is saved in local storage but expired
-    console.log("Previous authorization detected; access token is expired");
-    
-    updateAthleteInfo(JSON.parse(localStorage.getItem("strava_data")).athlete);
-
-    var refreshToken = JSON.parse(localStorage.getItem("strava_data")).refresh_token;
-    reAuthorize(refreshToken);
-}
-else if (queryString == ""){ // we don't have a code. They still need to log in and authorize
-    // encourage them to log in and authorize
-    console.log("No token in local storage, no authorization code");
-}
-else{ // we have a code because they logged in and authorized. the code can be found in the URL params
-    console.log("Authorization code retrieved")
-    var urlParams = new URLSearchParams(queryString);
-    var code = urlParams.get('code')
-    get_token();
-}
